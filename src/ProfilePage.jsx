2b23379898c5c2d4136c75207644b0a8199a2060
zsharon012 <zsharon012@gmail.com>
2026-01-02 17:14:14 -0600

    get and display profiles info from api

--- src/ProfilePage.jsx
+++ src/ProfilePage.jsx
@@ -1,56 +1,39 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import NavBar from './NavBar';
 import Footer from './Footer';
 import PersonalProfile from './PersonalProfile';
 import SavedProfile from './SavedProfile';
 
-const PROFILES = [
-  {
-    name: "Shelby Berry",
-    contactInfo: "hello@gmail.com",
-    clicked: false
-  },
-  {
-    name: "Shelby Berry",
-    contactInfo: "hello@gmail.com",
-    clicked: false
-  },
-  {
-    name: "Shelby Berry",
-    contactInfo: "hello@gmail.com",
-    clicked: false
-  },
-  {
-    name: "Shelby Berry",
-    contactInfo: "hello@gmail.com",
-    clicked: false
-  },
-  {
-    name: "Shelby Berry",
-    contactInfo: "hello@gmail.com",
-    clicked: false
-  },
-  {
-    name: "Shelby Berry",
-    contactInfo: "hello@gmail.com",
-    clicked: false
-  },
-  {
-    name: "Shelby Berry",
-    contactInfo: "hello@gmail.com",
-    clicked: false
-  }
-]
-
 export default function MainContent() {    
 
-    const [profiles, setProfiles] = useState(PROFILES);
+    const [profiles, setProfiles] = useState([]);
+    const [loading, setLoading] = useState(false);
 
     const handleClick = (idx) => {
         const newProfiles = [...profiles];
         newProfiles[idx].clicked = !newProfiles[idx].clicked;
         setProfiles(newProfiles);
     }
+
+    useEffect(() => {
+      setLoading(true);
+      const getAllUsers = async() => {
+        try {
+          const res = await fetch("https://disc-assignment-5-users-api-iyct.onrender.com/api/users");
+          const data = await res.json();
+          setProfiles(data);
+        } catch (e) {
+          console.error("error when fetching users", e);
+        } finally {
+          setLoading(false);
+        }
+      }
+
+      getAllUsers();
+    }, [])
+
+    if (loading) return <p>currently loading users...</p>
+    if (!profiles) return <p>no users found</p>
 
     return (
         <div className="everything">
@@ -64,14 +47,14 @@ export default function MainContent() {
                 <p>Saved People</p>
                 <p className="selected">Past Teammates</p>
                 </div>
-                <div>
+                <div className='single-profiles'>
                 {
                     profiles.map((profile, idx) => {
                     return (
                         <SavedProfile
                         key={idx}
-                        name={profile.name}
-                        contactInfo={profile.contactInfo}
+                        name={profile.firstName + profile.lastName}
+                        contactInfo={profile.email}
                         clicked={profile.clicked}
                         handleClick={() => {handleClick(idx)}}
                         />
